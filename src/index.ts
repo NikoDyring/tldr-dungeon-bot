54e9113d7d63afb204d7f5f08d96ed774258e82b
Nikolaj <Nikolaj@piboco.com>
2020-09-27 21:17:32 +0200

    Dragon lort

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import uncaught from 'uncaught'
 import log from './lib/log'
 import { setupClient } from './lib/client'
 
+log.info(`Starting bot in ${process.env.NODE_ENV} mode`)
+
 uncaught.start()
 uncaught.addListener(function (error: Error) {
   log.fatal(`Uncaught error or rejection: ${error.message}`)
